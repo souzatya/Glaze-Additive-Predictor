11902ae081cacd99bd232235eafcfa0375e9c924
Soujatya Sarkar <soujatya.sarkar2001@gmail.com>
2022-08-02 20:35:57 +0530

    windows

--- GAP_win/res/about.py
+++ GAP_win/res/about.py
@@ -13,8 +13,8 @@ def about():
     ab.iconbitmap(os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap','icon.ico'))
 
     Label(ab, text="Glaze Additive Predictor", font=("Heveltica Neue", 20, 'bold')).pack(pady=15)
-    Label(ab, text="Copyright © 2022, Soujatya Sarkar", font=("Heveltica Neue", 14, 'bold')).pack()
+    Label(ab, text="Copyright © 2022, Soujatya Sarkar, All Rights Reserved.", font=("Heveltica Neue", 12, 'bold')).pack()
     Label(ab, text="Computational Development and Research - Soujatya Sarkar").pack()
     Label(ab, text="Chemical Data Collection - Tapabrata Mondal and Soujatya Sarkar").pack()
-    Label(ab, text="Supervised by - Mr. Arindam Ghosh").pack()
-    Label(ab, text="Version - 1.0.0", font=("Heveltica Neue", 14, 'bold')).pack()
+    Label(ab, text="Author and Inventor - Soujatya Sarkar").pack()
+    Label(ab, text="Version - 1.0.0", font=("Heveltica Neue", 12, 'bold')).pack()
--- GAP_win/res/dependencies.py
+++ GAP_win/res/dependencies.py
@@ -6,7 +6,7 @@ def res_path(relative_path):
     try:
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = os.path.abspath("./res")
 
     return os.path.join(base_path, relative_path)
 
@@ -15,10 +15,10 @@ def check():
         os.mkdir(os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap'))
 
     if os.path.exists(os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap', 'icon.ico')) == False:
-        shutil.copy(res_path("res/icon.ico"),os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap'))
+        shutil.copy(res_path("icon.ico"),os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap'))
 
     if os.path.exists(os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap', 'logo.png')) == False:
-        shutil.copy(res_path("res/logo.png"),os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap'))
+        shutil.copy(res_path("logo.png"),os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap'))
 
     if os.path.exists(os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap', 'GLAZE_DATASET.xlsx')) == False:
-        shutil.copy(res_path("res/GLAZE_DATASET.xlsx"),os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap'))
+        shutil.copy(res_path("GLAZE_DATASET.xlsx"),os.path.join(os.path.expanduser('~'),'Documents','com.soujatya_sarkar.gap'))
